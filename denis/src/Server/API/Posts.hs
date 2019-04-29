6dd4ff611160a7a9018fa7e3c14cccd5a3665bbc
iko <ilyakooo0@gmail.com>
2019-04-29 17:32:19 +0300

    Posts/last Maybe ()

--- denis/src/Server/API/Posts.hs
+++ denis/src/Server/API/Posts.hs
@@ -23,7 +23,7 @@ import Server.Query.Pagination
 -- MARK: Implementation
 
 type PostApi = ReqBody '[JSON] [P.PostId] :> Post '[JSON] (ResponseWithUsers [P.Post]) :<|>
-    "last" :> ReqBody '[JSON] (PaginatingRequest P.PostId ()) :> Post '[JSON] (ResponseWithUsers [P.Post]) :<|>
+    "last" :> ReqBody '[JSON] (PaginatingRequest P.PostId (Maybe ())) :> Post '[JSON] (ResponseWithUsers [P.Post]) :<|>
     "forUser" :> ReqBody '[JSON] (PaginatingRequest P.PostId UserId) :> Post '[JSON] (ResponseWithUsers [P.Post]) :<|>
     "publish" :> ReqBody '[JSON] P.PostCreation :> Post '[JSON] P.PostId
 
@@ -33,7 +33,7 @@ postApi uId = getPostsApi :<|> lastPosts :<|> getPostsForUserApi :<|> publishPos
 getPostsApi :: [P.PostId] -> App (ResponseWithUsers [P.Post])
 getPostsApi = maybeNotFound . runQnotFound . getPosts
 
-lastPosts :: PaginatingRequest P.PostId () -> App (ResponseWithUsers [P.Post])
+lastPosts :: PaginatingRequest P.PostId (Maybe ()) -> App (ResponseWithUsers [P.Post])
 lastPosts (PaginatingRequest pId lim _) = maybeNotFound . runQnotFound $ getLastPosts pId lim
 
 getPostsForUserApi :: PaginatingRequest P.PostId UserId -> App (ResponseWithUsers [P.Post])
