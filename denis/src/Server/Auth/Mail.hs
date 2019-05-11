f9c223e6191ccd0b1a6234ebadab50f54c2070d9
iko <ilyakooo0@gmail.com>
2019-05-11 20:32:10 +0300

    Links in email are now valid.

--- denis/src/Server/Auth/Mail.hs
+++ denis/src/Server/Auth/Mail.hs
@@ -23,22 +23,44 @@ import Control.Monad.IO.Class
 import Network.Mail.Mime (Mail, Part)
 import Data.Text (Text)
 import qualified Data.Text.Lazy as TL
+import Text.Blaze.Html5 as H
+import Text.Blaze.XHtml5.Attributes as A
+import Text.Blaze.Html.Renderer.Text
+import Data.ByteString (ByteString)
+import Data.User
+import Data.ByteString.Char8 (unpack)
 
-sendTokenVerificationEmail :: TokenVerificationCode -> TL.Text -> Text -> App ()
-sendTokenVerificationEmail code ua email = do
-    mailCfg <- mailConfig <$> ask
+sendTokenVerificationEmail :: TokenVerificationCode -> UserId -> ByteString -> ByteString -> TL.Text -> Text -> App ()
+sendTokenVerificationEmail userCode uId activation deactivation ua email = do
+    cfg <- ask
+    let mailCfg = mailConfig cfg
+    let rootUrl = selfRootUrl cfg
     let mail = simpleMail
             (Address (Just "HSE Social Network") "noreply@hse.ru")
             [Address Nothing email] [] []
             "HSE Social Network Verification Code"
-            [genMail code ua]
+            [genMail rootUrl userCode uId (unpack activation) (unpack deactivation) ua]
     sendMailWithConfig mailCfg mail
 
 sendMailWithConfig :: (MonadIO m) => MailConfig -> Mail -> m ()
 sendMailWithConfig (MailConfig host port user pass) =
     liftIO . sendMailWithLogin' host (fromIntegral port) user pass
 
-genMail :: TokenVerificationCode -> TL.Text -> Part
-genMail code ua = htmlPart $ "<html><body style=\"font-family: sans-serif;\"> <h1> <div align=\"center\" style=\"margin-top: 55px;\"> <a href=\"#\" style=\"font-size: 34px; font-weight: bold; display: inline-block; padding: 7px 13px 7px 13px; margin-bottom: 89px; text-decoration: underline; font-family: sans-serif; color: #555;\"> Log in </a> <br /> <div style=\"font-size: 34px; font-weight: bold;\"> <span style=\"color: #bbb; font-weight: bold;\">from: </span>" <> ua <> "</div> <br/> <div style=\"font-size: 55px; font-weight: bold; border-radius: 10px; background-color: #eee; display: inline-block; padding: 7px 13px 7px 13px; box-shadow: 0px 5px 21px #bbb; background-image: linear-gradient(#f9f9f9, #eee);\">" <> TL.pack [a, b] <> "<div style=\"display: inline-block; width: 13px;\"></div>" <> TL.pack [c, d] <> "<div style=\"display: inline-block; width: 13px;\"></div>" <> TL.pack [e, f] <> "</div></div><div align=\"center\" style=\"margin-top: 233px;\"> <a href=\"#\" style=\"text-decoration: underline; font-family: sans-serif; color: #555;font-size: 13px; margin-bottom: 21px;\">Revoke access</a> </div></body></html>"
+genMail :: String -> TokenVerificationCode -> UserId -> String -> String -> TL.Text -> Part
+genMail rootUrl userCode uId activation deactivation ua = htmlPart . renderHtml $ html $ body ! A.style "font-family: sans-serif;" $ do
+    H.div ! A.style "text-align: center; margin-top: 55px;" $ do
+        H.a ! href (stringValue $ rootUrl <> "/authentication/activateUser?id=" <> show uId <> "&data=" <> activation) ! A.style "font-size: 34px; font-weight: bold; display: inline-block; padding: 7px 13px 7px 13px; margin-bottom: 89px; text-decoration: underline; font-family: sans-serif; color: #555;" $ "Log in"
+        br
+        H.div ! A.style "font-size: 34px; font-weight: bold;" $ do
+            H.span ! A.style "color: #bbb; font-weight: bold;" $ "from: "
+            toHtml ua
+        br
+        H.div ! A.style "font-size: 55px; font-weight: bold; border-radius: 10px; background-color: #eee; display: inline-block; padding: 7px 13px 7px 13px; box-shadow: 0px 5px 21px #bbb; background-image: linear-gradient(#f9f9f9, #eee);" $ do
+            toHtml [a', b']
+            H.div ! A.style "display: inline-block; width: 13px;" $ mempty
+            toHtml [c', d']
+            H.div ! A.style "display: inline-block; width: 13px;" $ mempty
+            toHtml [e', f']
+    H.div ! A.style "text-align: center; margin-top: 233px;" $ H.a ! href (stringValue $ rootUrl <> "/authentication/deactivateUser?id=" <> show uId <> "&data=" <> deactivation) ! A.style "text-decoration: underline; font-family: sans-serif; color: #555;font-size: 13px; margin-bottom: 21px;" $ "Revoke access"
     where
-        [a, b, c, d, e, f] = reverse . take 6 . (++ repeat '0') . reverse . show $ code
+        [a', b', c', d', e', f'] = reverse . take 6 . (++ repeat '0') . reverse . show $ userCode
