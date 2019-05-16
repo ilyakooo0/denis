07853523054cfc49e4053c683f4c3e1314fe041f
iko <ilyakooo0@gmail.com>
2019-05-17 02:27:29 +0300

    Index

--- denis/sql/indexes.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE FUNCTION my_array_to_text(text[])
-  RETURNS text LANGUAGE sql IMMUTABLE AS $$SELECT array_to_string($1, ' ')$$;
-
-CREATE INDEX "faculty_text" ON "faculties" USING GIN (to_tsvector('russian', "facultyName" || ' ' || "facultyPath" || ' ' || "facultyCampusName" || ' ' || my_array_to_text("facultyTags")))
-
